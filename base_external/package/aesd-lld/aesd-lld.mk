21e1f0e97d5f7a7c15ffdc841d61ae0304c2d969
bmoussa1 <bilalmoussa0@gmail.com>
2025-02-28 19:37:56 +0200

    test

---
+++ base_external/package/aesd-lld/aesd-lld.mk
@@ -0,0 +1,17 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+AESD_LLD_VERSION = 'e09571f255cfd29143db32718fa00bac56657715'
+AESD_LLD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-bmoussa1.git'
+AESD_LLD_SITE_METHOD = git
+AESD_LLD_GIT_SUBMODULES = YES
+
+AESD_LLD_MODULE_SUBDIRS = misc-modules scull
+AESD_LLD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
